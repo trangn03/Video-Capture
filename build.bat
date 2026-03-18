45cbd95ee868e55f19d9e75c480400699e02c14c
trangn03 <xtrang0201@gmail.com>
2026-03-18 08:42:22 -0700

    Update exe file

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 echo Deleting old versions...
 rd /s /q build
 rd /s /q dist
+echo Activate virtual environment
+call venv/Scripts/activate
 echo Building new EXE...
 pyinstaller --onefile --windowed capture.py
 echo Done! Your new app is in the "dist" folder.
