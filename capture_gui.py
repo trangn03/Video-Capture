4c25851c8ca5f6c2cc01a7c4aefc13df124964cf
trangn03 <xtrang0201@gmail.com>
2026-07-17 07:40:21 -0700

    Added font object for the UI

--- capture_gui.py
+++ capture_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font
 import capture
 
 class CaptureGUI:
@@ -7,34 +7,37 @@ class CaptureGUI:
         # Window set up
         self.root = tk.Tk()
         self.root.title("Capture Dashboard")
-        self.root.geometry("500x500")
+        self.root.geometry("550x550")
+        
+        # Create a custom font object
+        text_font = font.Font(family="Segoe UI", size= 18)
         
         # Entering part number
-        self.label_part = tk.Label(self.root, text = "Enter PART NUMBER:", font=('Arial', 18))
+        self.label_part = tk.Label(self.root, text = "Enter PART NUMBER:", font=text_font)
         self.label_part.pack(padx=10, pady=10)
         
-        self.entry_part = tk.Entry(self.root, font=('Arial', 18), justify="center")
+        self.entry_part = tk.Entry(self.root, font=text_font, justify="center")
         self.entry_part.pack(padx=10, pady=10)
         
         # Entering job number 
-        self.label_job = tk.Label(self.root, text = "Enter JOB NUMBER:", font=('Arial', 18))
+        self.label_job = tk.Label(self.root, text = "Enter JOB NUMBER:", font=text_font)
         self.label_job.pack(padx=10, pady=10)
         
-        self.entry_job = tk.Entry(self.root, font=('Arial', 18), justify="center")
+        self.entry_job = tk.Entry(self.root, font=text_font, justify="center")
         self.entry_job.pack(padx=10, pady=10)
         
         # Entering serial number
-        self.label_sn = tk.Label(self.root, text="Enter SERIAL NUMEBRS one per line: ", font=('Arial', 18))
+        self.label_sn = tk.Label(self.root, text="Enter SERIAL NUMEBRS one per line: ", font=text_font)
         self.label_sn.pack(padx=10, pady=10)
         
-        self.text_sn = tk.Text(self.root, height = 5, width=30, font=('Arial', 18))
+        self.text_sn = tk.Text(self.root, height = 5, width=30, font=text_font)
         self.text_sn.pack(padx=10, pady=10)
         
         # Start the capture button
         self.btn_start = tk.Button (
             self.root, 
             text="Start Capture",
-            font=('Arial', 18),
+            font=text_font,
             bg="#4CAF50",
             fg="black",
         )
